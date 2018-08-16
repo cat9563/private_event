5deb16b66e3dec2f89074d3d88ddfbf80e2ea4c9
Crystal Ramirez <crystal9563@gmail.com>
2018-08-16 14:55:54 -0400

    user model and create sheet completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  root 'static_pages#home'
   get 'static_pages/home'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
