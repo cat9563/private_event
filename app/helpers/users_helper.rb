1bcdbdcdeb0ff365bdb49e2821b508cc4b480123
Crystal Ramirez <crystal9563@gmail.com>
2018-08-23 13:57:10 -0400

    finished basic event

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,10 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  #Returns the list of events user created
+  def list_events(user)
+    @events.each do |event|
+    event.title
+  end
 end
